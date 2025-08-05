61b63607a21dbb172e16a1ca672661ebd6e41cff
Aluno.Etec <aula@etec.com>
2025-08-04 22:37:47 -0300

    Exercicios de fixação de php

---
+++ php/execicios.php
@@ -0,0 +1,41 @@
+<html>
+<header>
+<title>Aula 1</title>
+</header>
+<body>
+<?php
+$a = 6;
+$b = 435;
+$c = 2;
+$fatorial=0;
+
+
+if($a>$b && $a>$c)
+	echo "O valor ".$a." é o maior <br>";
+else if($b>$a && $b>$c)
+	echo "O valor ".$b." é o maior <br>";
+else if($c>$a && $c>$b)
+	echo "O valor ".$c." é o maior <br>";
+
+
+if($a<$b && $b<$c)
+	echo $a."-".$b."-".$c;
+else if($a<$c && $c<$b)
+	echo $a."-".$c."-".$b;
+else if($b<$a && $a<$c)
+	echo $b."-".$a."-".$c;
+else if($b<$c && $c<$a)
+	echo $b."-".$c."-".$a;
+else if($c<$a && $a<$b)
+	echo $c."-".$a."-".$b;
+else if($c<$b && $b<$a)
+	echo $c."-".$b."-".$a;
+
+if($a/$a==0)
+	echo "<br>Esse numero ".$a." é primo";
+
+for($i=$c; $i>0; $i--){
+	$fatorial+=$c*$i;
+}
+echo "<br>O fatorial de ".$c." é ".$fatorial;
+?>
