ddc411c4701483952e69bef74ea1f1cb823c5e3d
Aluno.Etec <aula@etec.com>
2025-08-05 21:10:58 -0300

    introdução

--- php/introducao.php
+++ php/introducao.php
@@ -1,13 +1,27 @@
+<!DOCTYPE html>
 <html>
-<header>
-<title>Aula 1</title>
-</header>
+<head>
+	<title>Aula 1 - Introdução PHP</title>
+</head>
 <body>
 <?php
-$valor1 = 10;
-$valor2 = 10;
-$total = $valor1 + $valor2;
-
-echo "O valor total é igual a: " . $total;
-
+	$valor1 = -10;
+	$valor2 = 20;
+	$total = $valor1 + $valor2;
+	$positivo = "sim";
+	
+	if($valor1 < 0){
+		echo "valor1 é negativo <Br><br>";
+	}
+	else{
+		echo "valor1 é positivo <Br><br>";
+	}
+	
+	for($i = 1; $i <= 10; $i++){
+		echo "rodada " . $i . "<br>";
+	}
+	
+	echo "<br><br><br><h1>" . $total . "</h1>";
 ?>
+</body>
+</html>
