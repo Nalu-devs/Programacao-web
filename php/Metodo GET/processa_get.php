4f492ea2a6c01e34204b95824e6663069f989d47
Aluno.Etec <aula@etec.com>
2025-08-19 22:18:32 -0300

    Aqui pegamos o formulario e enviamos com metodo GET

---
+++ php/Metodo GET/processa_get.php
@@ -0,0 +1,10 @@
+<?php
+
+$nome = $_GET["nome"];
+$email = $_GET["email"];
+$idade = $_GET["idade"];
+$faixa_etaria = $_GET["faixa_etaria"];
+
+echo "Seja bem-vindo $nome!<br>Sua idade é: $idade<br>E sua faixa etaria: $faixa_etaria";
+
+?>
